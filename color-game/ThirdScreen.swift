45cd13086c864cc042c162ad40453ca460a849da
guilschaidhauer <e.guilherme.castro@gmail.com>
2023-12-26 19:41:24 +0100

    Add third screen

---
+++ color-game/ThirdScreen.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdScreen.swift
+//  color-game
+//
+//  Created by Guilherme Schaidhauer de Castro on 26/12/23.
+//
+
+import UIKit
+
+class ThirdScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(red: 255/255, green: 166/255, blue: 0/255, alpha: 1)
+    }
+}
